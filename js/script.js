6e2d97aaec205634ce79b90b2883d3588e573a5e
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-24 18:46:39 +0100

    modify drawCard() template

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ Milestone 2
 Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 Milestone 3
 Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user).
-Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.BONUS
+Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+BONUS
 1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
 2- popolare le options della select della milestone 3 dinamicamente.
 */
 
-// array di oggetti 'icons'
 const icons = [
     {
         name: 'cat',
@@ -132,9 +132,9 @@ function drawCard() {
         const row = document.querySelector('.row');
         let cardContent = '';
         const template = `
-        <div class="col">
+        <div class="col-12 col-sm-6 col-md-4 col-lg-3">
             <div class="card">
-                <div class="cardBody d-flex flex-column text-center p-3">
+                <div class="cardBody d-flex flex-column text-center p-3" style="color:${value.color}">
                     <i class="${value.prefix}${value.family} ${value.prefix}${value.name} fs-1"></i>
                     <div class="text-uppercase">${value.name}</div>
                 </div>
